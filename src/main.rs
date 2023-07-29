8c7d1e9d795c314df9f24877575acfb7ae0ed983
Regalia <regaliaxyz@gmail.com>
2023-07-29 17:47:21 +0200

    basic json returns & catchers implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use rocket::serde::json::{json, Value};
+
+
 #[macro_use] extern crate rocket;
 
 
@@ -6,7 +9,37 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/hello/<name>")]
+fn print_name(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[get("/json/<name>")]
+fn print_json(name: &str) -> Value {
+    json!({
+        "name": name,
+    })
+}
+
+#[catch(404)]
+fn not_found() -> Value {
+    json!({
+        "status": "error",
+        "reason": "Resource was not found."
+    })
+}
+
+#[catch(500)]
+fn internal_error() -> Value {
+    json!({
+        "status": "error",
+        "reason": "Internal server error."
+    })
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index, print_name, print_json])
+        .register("/", catchers![not_found, internal_error])
 }
